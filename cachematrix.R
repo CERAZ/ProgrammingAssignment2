59af4e0401c5c44001095a5dd2e926393b991dd1
James Carter <James Carter@users.noreply.github.com>
2015-10-23 20:18:20 -0600

    Description of the two functions purposes.
    makeCacheMatrix -- creates a matrix that can be cached.
    cacheSolve -- computes the inverse matrix of "makeCacheMatrix".  It
    determines if the inverse has already been calculated (and the matrix
    has not changed), then the "cacheSolve" should retrieve the matrix from
    the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -7,14 +7,22 @@
 
 ## Write a short comment describing this function --
 
-## 
+## makeCacheMatrix -- This function creates a matrix from a data object such 
+## as a vector/array/factor/frame and sets two environments.  One environment
+## is for creating the matrix from the object and the other environment is
+## for storage of the created matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    
 }
 
 
 ## Write a short comment describing this function
+
+## cacheSolve -- This function takes the cached martix created by "makeCache
+## Matrix" and renders an inverse matrix for the variable 'x' in the "cache
+## Solve" function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
